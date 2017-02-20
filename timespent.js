f83d6276d2f83872d2a3713e334de299f093a75b
Jordi Prats <jprats@systemadmin.es>
2017-02-20 23:07:38 +0100

    Update timespent.js

--- timespent.js
+++ timespent.js
@@ -10,7 +10,7 @@ window.addEventListener('load', function() {
   
   var script = document.createElement('script');
   
-  script.innerHTML = 'var timeStamp = Math.floor(Date.now() / 1000);function wasted() { var nouu = Math.floor(Date.now() / 1000); document.getElementsByName("dbfield:3:__raw_custom__:__single__:log_work_time_spent")[0].value=Math.floor(((nouu-timeStamp)/60)+1)+"m"; setTimeout( "wasted ()", 60000 );} wasted();';
+  script.innerHTML = 'var timeStamp = Math.floor(Date.now() / 1000);function wasted() { var nouu = Math.floor(Date.now() / 1000); document.getElementsByName("dbfield:3:__raw_custom__:__single__:log_work_time_spent")[0].value=Math.floor(((nouu-timeStamp)/60)+1)+"m"; setTimeout( "wasted ()", 61000 );} wasted();';
 
   x[0].appendChild(script);
 
